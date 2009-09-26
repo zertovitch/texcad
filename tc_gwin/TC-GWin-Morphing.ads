80b657bfb548da11373efbfa4f1521c16dba8d51
gdemont <gdemont@aa1f1193-9ea0-42dc-a717-61ff1ef4fd01>
2009-09-26 20:05:43 +0000

    Removed upper-case duplicates

--- tc_gwin/TC-GWin-Morphing.ads
+++
@@ -1,7 +0,0 @@
-with TC.GWin.MDI_Picture_Child;         use TC.GWin.MDI_Picture_Child;
-
-package TC.GWin.Morphing is
-
-  procedure Deformation(w: in out TC_Picture_Panel);
-
-end TC.GWin.Morphing;
