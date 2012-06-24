3a93ce36be099201faba67569c00dcb7dc2281e6
gdemont <gdemont@aa1f1193-9ea0-42dc-a717-61ff1ef4fd01>
2012-06-24 11:24:22 +0000

    Some debugging effort around GNAT GPL 2011 & the destruction of MDI children

--- tc_gwin/build_debug.cmd
+++ tc_gwin/build_debug.cmd
@@ -2,9 +2,6 @@ rem *********************************************************
 rem BLOATED EXTRA-SECURE DEBUGGING:
 rem *********************************************************
 
-del texcad.exe
-
 gnatmake -Ptexcad_gps_win32 -XBuild_Mode=Debug
 
-ren texcad.exe TeXCAD.exe
-copy /b texcad.exe TeXCAD_debug.exe
+copy /b ..\acu_debg\texcad.exe TeXCAD_debug.exe
--- tc_gwin/build_optim.cmd
+++ tc_gwin/build_optim.cmd
@@ -2,11 +2,8 @@ rem *********************************************************
 rem OPTIMIZED IN SPACE (excepted explicit inlining) AND TIME:
 rem *********************************************************
 
-del texcad.exe
-
 gnatmake -Ptexcad_gps_win32 -XBuild_Mode=Optimize
 
-ren texcad.exe TeXCAD.exe
-copy /b texcad.exe TeXCAD_optim.exe
+copy /b ..\acu_opti\texcad.exe TeXCAD_optim.exe
 
 rem upx --ultra-brute TeXCAD_optim.exe
