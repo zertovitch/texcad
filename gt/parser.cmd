5ba5d8ff0ab6a88a2d78bf3cb7c1adcece4fc5e2
gdemont <gdemont@aa1f1193-9ea0-42dc-a717-61ff1ef4fd01>
2014-02-26 23:26:05 +0000

    GT2PIC: added comments to parser generator script

--- gt/parser.cmd
+++ gt/parser.cmd
@@ -1,3 +1,9 @@
+@echo off
+echo Build the GT (graphthing) parser from GT.y (parser grammar) and GT.l (lexer).
+echo You need GNAT, AYACC and AFLEX visible in the PATH.
+echo AYACC needs to be the version with the Prefix_All option (20-Nov-2013 or later).
+echo AYACC is available with the Pascal-to-Ada translator @ http://p2ada.sf.net/ .
+
 ayacc gt.y Off Off On Off Off .a On
 gnatchop -w gt.a
 
@@ -12,3 +18,5 @@ del gt_dfa.a
 del gt.a
 
 gnatmake -P gt
+
+pause
