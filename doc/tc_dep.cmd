ba2ccab8086a88ea88d7fc0fa3642aae30edebfd
gdemont <gdemont@aa1f1193-9ea0-42dc-a717-61ff1ef4fd01>
2014-06-01 19:32:02 +0000

    Updated tc_dep.cmd (dependency graph command)

--- doc/tc_dep.cmd
+++ doc/tc_dep.cmd
@@ -4,8 +4,13 @@ rem   Create a DOT ( http://www.graphviz.org/ ) file for displaying
 rem   TeXCAD's unit dependency structure as a directed graph.
 rem   The tool is called DePlo, http://sites.google.com/site/depplot/
 
+rem Refresh the library files through a fresh build
+gnatmake -P ../tc_gwin/texcad_gwin
+
+rem Run DePlo
 deplo config=tc_dep.cfg >tc_dep.dot
 
+rem Run DOT, a Graphviz tool
 dot -o tc_dep.emf -Temf tc_dep.dot
 
 pause
