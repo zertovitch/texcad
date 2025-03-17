09c9ff487aeef0ab758fbdc79d587046b67cc959
unknown <gdemont@hotmail.com>
2025-03-17 21:04:31 +0100

    Added a test program for inputs (w.i.p.)

---
+++ test_io/test_tc_input.adb
@@ -0,0 +1,12 @@
+with TC.Input;
+
+procedure Test_TC_Input is
+  procedure Test (name : String) is
+    pic: TC.Picture;
+  begin
+    TC.Input.Load (pic, False, name & ".tcp");
+  end Test;
+
+begin
+  Test ("test_io/LineThrs");
+end Test_TC_Input;
